a6aadc3293f96451fd685a0a165921cd562bd49d
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-05-17 12:26:29 +0200

    Declare templates as template string

--- src/lib/generateSimulationFile.ts
+++ src/lib/generateSimulationFile.ts
@@ -8,53 +8,53 @@ import { Journey } from './types/journey';
 import { Request } from './types/simulation';
 
 export type SimulationParams = {
-    readonly simulationName: string,
-    readonly packageName: string,
-    readonly scenarioName: string,
-    readonly baseUrl: string,
-    readonly maxUsersCount: number,
-    readonly requests: ReadonlyArray<Request>,
+  readonly simulationName: string,
+  readonly packageName: string,
+  readonly scenarioName: string,
+  readonly baseUrl: string,
+  readonly maxUsersCount: number,
+  readonly requests: ReadonlyArray<Request>,
 }
 
 export const generateSimulations = () => {
-    const { baseUrl, dir, logs, packageName, errMessage } = cli();
+  const { baseUrl, dir, logs, packageName, errMessage } = cli();
 
-    if (errMessage) {
-        return console.error(`Error: ${errMessage}`);
+  if (errMessage) {
+    return console.error(`Error: ${errMessage}`);
+  }
+
+  const jsonInDir = fs.readdirSync(dir).filter(file => path.extname(file) === '.json');
+  console.log(`Found ${jsonInDir.length} json files in path: ${jsonInDir}`);
+
+  jsonInDir.forEach(file => {
+    const journey: Journey = JSON.parse(fs.readFileSync(path.resolve(dir, file)).toString());
+    // const requests = parseTraces(journey.traceItems.filter(tr => tr.transactionType === 'request'));
+    const requests = getHttpRequests(journey.traceItems);
+
+    if (logs) {
+      requests.forEach(req => console.log(`${req.date} ${req.transactionId} ${req.method} ${req.path}`))
     }
 
-    const jsonInDir = fs.readdirSync(dir).filter(file => path.extname(file) === '.json');
-    console.log(`Found ${jsonInDir.length} json files in path: ${jsonInDir}`);
+    const simulationName = `${journey.journeyName.replace(/\s/g, "")}`;
+    const filename = `${simulationName}.scala`;
+    const outputDir = path.resolve('output');
+    const filePath = path.resolve(outputDir, filename);
 
-    jsonInDir.forEach(file => {
-        const journey: Journey = JSON.parse(fs.readFileSync(path.resolve(dir, file)).toString());
-        // const requests = parseTraces(journey.traceItems.filter(tr => tr.transactionType === 'request'));
-        const requests = getHttpRequests(journey.traceItems);
+    const simulationContent = buildSimulation({
+      simulationName,
+      packageName,
+      scenarioName: `${journey.journeyName} ${journey.kibanaVersion}`,
+      baseUrl: `${baseUrl.protocol}//${baseUrl.host}`,
+      maxUsersCount: journey.maxUsersCount,
+      requests: requests
+    });
 
-        if (logs) {
-            requests.forEach(req => console.log(`${req.date} ${req.transactionId} ${req.method} ${req.path}`))
-        }
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
 
-        const simulationName = `${journey.journeyName.replace(/\s/g, "")}`;
-        const filename = `${simulationName}.scala`;
-        const outputDir = path.resolve('output');
-        const filePath = path.resolve(outputDir, filename);
-
-        const simulationContent = buildSimulation({
-            simulationName,
-            packageName,
-            scenarioName: `${journey.journeyName} ${journey.kibanaVersion}`,
-            baseUrl: `${baseUrl.protocol}//${baseUrl.host}`,
-            maxUsersCount: journey.maxUsersCount,
-            requests: requests
-        });
-
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir);
-        }
-
-        const stream = fs.createWriteStream(filePath);
-        stream.write(simulationContent);
-        stream.end(() => console.log(`Gatling simulation '${filename}' was saved`));
-    })
+    const stream = fs.createWriteStream(filePath);
+    stream.write(simulationContent);
+    stream.end(() => console.log(`Gatling simulation '${filename}' was saved`));
+  });
 }
